86e92d2b95fd0f972b62bc4af7dd9967a12e333e
González Vázquez Jorge Alberto <jgonzva140295@gmail.com>
2020-11-29 23:55:40 -0600

    version 1

---
+++ app/src/main/java/com/example/tarea1unidad2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tarea1unidad2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv1=(WebView) findViewById(R.id.web);
+        wv1.setWebViewClient(new WebViewClient());
+        wv1.loadUrl("https://www.google.com/");
+    }
+}
